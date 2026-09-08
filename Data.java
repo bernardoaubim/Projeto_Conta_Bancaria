202f5e46a93cedff389155fa23a12ed9b1e77850
Bernardo Aubim Barbosa <bernardoaubim@gmail.com>
2026-09-08 20:06:21 -0300

    Create Data class

---
+++ Data.java
@@ -0,0 +1,28 @@
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d/%02d/%04d", dia, mes, ano);
+    }
+}
